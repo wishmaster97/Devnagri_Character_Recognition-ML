eb5f5e32a54ae6fe6230afc4854a86ddfc99b4ee
wishmaster97 <36235341+wishmaster97@users.noreply.github.com>
2020-02-09 20:50:38 +0530

    Update webcamTraining.py

--- webcamTraining.py
+++ webcamTraining.py
@@ -1,164 +1,88 @@
-import cv2
-from keras.models import load_model
 import numpy as np
-from collections import deque
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-class DevReg:
-    def webCam(self):
-        model = load_model('devnagri.h5')
-        print(model)
+from keras import layers
+from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalAveragePooling2D, GlobalMaxPooling2D
+from keras.utils import np_utils, print_summary
+from keras import optimizers
+import pandas as pd
+from keras.models import Sequential
+from keras.callbacks import ModelCheckpoint
+import keras.backend as K
 
-        letter_count = {0: 'CHECK', 1:'01_ka', 2:'02_kha', 3:'03_ga', 4: '04_gha', 5:'05_kna', 6:'06_cha',
-                        7:'07_chha', 8:'08_ja', 9:'09_jha', 10:'10_yna',
-                        11: '11_tta(Tamatar)', 12: '12_ttha', 13: '13_dda', 14: '14_ddha', 15: '15_adna', 16: '16_ta(Tabla)', 17: '17_tha',
-                        18: '18_da', 19: '19_dha', 20: '20_na', 21: '21_pa', 22: '22_pha', 23: '23_ba', 24: '24_bha', 25: '25_ma',
-                        26: '26_yaw(yash)',
-                        27: '27_ra', 28: '28_la', 29: '29_va (veer)',30: '30_sha (Shalgam)',
-                        31: '31_pa', 32: '32_sa', 33: '33_ha', 34: '34_chhya', 35: '35:tra', 36: '36_gya',37: 'CHECK'}
-        """,37:'0',
-                     38:'1',
-                     39:'2',
-                     40:'3',
-                     41:'4',
-                     42:'5',
-                     43:'6',
-                     44:'7',
-                     45:'8',
-                     46:'9', 47: 'CHECK'}"""
+data_train = pd.read_csv("devnagridata.csv")
+data_test = pd.read_csv("datatest.csv")
+dataset_train = np.array(data_train)
+dataset_test = np.array(data_test)
+np.random.shuffle(dataset_train)
+np.random.shuffle(dataset_test)
 
-        def keras_predict(model, image):
-            processed = keras_process_image(image)
-            print("processed: "+str(processed.shape))
-            pred_probab = model.predict(processed)[0]
-            pred_class = list(pred_probab).index(max(pred_probab))
-            return max(pred_probab), pred_class
+X = dataset_train
+Y = dataset_train
+X = X[:, 0:1024]
+Y = Y[:, 1024]
 
-        def keras_process_image(img):
-            image_x = 32
-            image_y = 32
-            img = cv2.resize(img,(image_x,image_y))
-            img = np.array(img, dtype = np.float32)
-            img = np.reshape(img, (-1, image_x, image_y, 1))
-            print(img)
-            type(img)
-            return img
+X_t = dataset_test
+Y_t = dataset_test
+X_t = X_t[:, 0:1024]
+Y_t = Y_t[:, 1024]
 
-        try:
-            cap = cv2.VideoCapture(0)
-            Lower_blue = np.array([110, 50, 50])
-            Upper_blue = np.array([130, 255, 255])
-            pred_class = 0
-            pts = deque(maxlen=5120000)
-            blackboard = np.zeros((480, 640, 3), dtype=np.uint8)
-            digit = np.zeros((200, 200, 3), dtype=np.uint8)
-            while cap.isOpened():
-                ret, img = cap.read()
-                img = cv2.flip(img, 1)
-                imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-                mask = cv2.inRange(imgHSV, Lower_blue, Upper_blue)
-                blur = cv2.medianBlur(mask, 15)
-                blur = cv2.GaussianBlur(blur, (5, 5), 0)
-                thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-                cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
-                center = None
-                if len(cnts) >= 1:
-                    contour = max(cnts, key=cv2.contourArea)
-                    if cv2.contourArea(contour) > 250:
-                        ((x, y), radius) = cv2.minEnclosingCircle(contour)
-                        cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                        cv2.circle(img, center, 5, (0, 0, 255), -1)
-                        M = cv2.moments(contour)
-                        center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
-                        pts.appendleft(center)
-                        for i in range(1, len(pts)):
-                            if pts[i - 1] is None or pts[i] is None:
-                                continue
-                            cv2.line(blackboard, pts[i - 1], pts[i], (255, 255, 255), 10)
-                            cv2.line(img, pts[i - 1], pts[i], (0, 0, 255), 5)
-                elif len(cnts) == 0:
-                    if len(pts) != []:
-                        blackboard_gray = cv2.cvtColor(blackboard, cv2.COLOR_BGR2GRAY)
-                        blur1 = cv2.medianBlur(blackboard_gray, 15)
-                        blur1 = cv2.GaussianBlur(blur1, (5, 5), 0)
-                        thresh1 = cv2.threshold(blur1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-                        blackboard_cnts = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
-                        if len(blackboard_cnts) >= 1:
-                            cnt = max(blackboard_cnts, key=cv2.contourArea)
-                            print(cv2.contourArea(cnt))
-                            if cv2.contourArea(cnt) > 2000:
-                                x, y, w, h = cv2.boundingRect(cnt)
-                                digit = blackboard_gray[y:y + h, x:x + w]
-                                # new Image = process_letter(digit)
-                                pred_probab, pred_class = keras_predict(model, digit)
-                                print(pred_class, pred_probab)
+X_train = X[0:72001, :]
+X_train = X_train / 255.
+X_test = X_t[0:21601, :]
+X_test = X_test/ 255.
 
-                    pts = deque(maxlen=512)
-                    blackboard = np.zeros((480, 640, 3), dtype=np.uint8)
-                cv2.putText(img, "Conv Network: " + str(letter_count[pred_class]), (10, 470),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                cv2.imshow("Frame", img)
-                cv2.imshow("Contour", thresh)
-                k = cv2.waitKey(10)
-                if k ==27:
-                    break
+#Reshape
+Y = Y.reshape(Y.shape[0], 1)
+Y_t = Y_t.reshape(Y_t.shape[0], 1)
+Y_train = Y[0:72001, :]
+Y_train = Y_train.T
+Y_test  = Y_t[0:21601, :]
+Y_test  = Y_test.T
 
-        except cv2.error as e:
-            pass
+print("Number of Training examples = "+str(X_train.shape[0]))
+print("Number of test examples = "+str(X_test.shape[0]))
+print("X_train shape: "+str(X_train.shape))
+print("Y_train shape: "+str(Y_train.shape))
+print("X_test shape: "+str(X_test.shape))
+print("Y_test shape: "+str(Y_test.shape))
 
-    def canvasReg(self):
-        model = load_model('devnagri.h5')
-        print(model)
+image_x = 32
+image_y = 32
 
-        letter_count = {0: 'CHECK', 1: '01_ka', 2: '02_kha', 3: '03_ga', 4: '04_gha', 5: '05_kna', 6: '06_cha',
-                        7: '07_chha', 8: '08_ja', 9: '09_jha', 10: '10_yna',
-                        11: '11_tta(Tamatar)', 12: '12_ttha', 13: '13_dda', 14: '14_ddha', 15: '15_adna',
-                        16: '16_ta(Tabla)', 17: '17_ttha',
-                        18: '18_da', 19: '19_dha', 20: '20_na', 21: '21_pa', 22: '22_pha', 23: '23_ba', 24: '24_bha',
-                        25: '25_ma',
-                        26: '26_yaw(yash)',
-                        27: '27_ra', 28: '28_la', 29: '29_waw(veer)', 30: '30_sha (Shalgam)',
-                        31: '31_pa', 32: '32_sa', 33: '33_ha', 34: '34_ga', 35: '35:tra', 36: '36_gya',
-                        37: 'CHECK'}
-        """,37:'0',
-                     38:'1',
-                     39:'2',
-                     40:'3',
-                     41:'4',
-                     42:'5',
-                     43:'6',
-                     44:'7',
-                     45:'8',
-                     46:'9', 47: 'CHECK'}"""
+train_y = np_utils.to_categorical(Y_train)
+train_y = train_y.reshape(train_y.shape[1], train_y.shape[2])
+test_y  = np_utils.to_categorical(Y_test)
+test_y  = test_y.reshape(test_y.shape[1], test_y.shape[2])
+X_train = X_train.reshape(X_train.shape[0], image_x, image_y, 1)
+X_test  = X_test.reshape(X_test.shape[0], image_x, image_y, 1)
 
-        def keras_predict(model, image):
-            processed = keras_process_image(image)
-            print("processed: " + str(processed.shape))
-            pred_probab = model.predict(processed)[0]
-            pred_class = list(pred_probab).index(max(pred_probab))
-            print("KProcess2")
-            return max(pred_probab), pred_class
+print("X_train shape: "+str(X_train.shape))
+print("Y_train shape: "+str(train_y.shape))
 
-        def keras_process_image(img):
-            image_x = 32
-            image_y = 32
-            img = cv2.resize(img, (image_x, image_y))
-            img = np.array(img, dtype=np.float32)
-            img = np.reshape(img, (-1, image_x, image_y, 1))
-            print("KProcess")
-            print(img)
-            return img
+#Buildinig A Model
 
-        pred_class = 0
-        rgb = cv2.imread("./temp.png")
-        gray =  cv2.cvtColor(rgb,cv2.COLOR_RGB2GRAY)
-        #binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+def keras_model(image_x,image_y):
+    num_of_classes = 37
+    model = Sequential()
+    model.add(Conv2D(filters =32, kernel_size =(5,5), input_shape = (image_x, image_y, 1), activation ='relu'))
+    model.add(MaxPooling2D(pool_size =(2,2), strides=(2,2), padding='same'))
+    model.add(Conv2D(64,(5,5), activation = 'relu'))
+    model.add(MaxPooling2D(pool_size = (5,5), strides=(5,5), padding ='same'))
+    model.add(Flatten())
+    model.add(Dense(num_of_classes, activation = 'softmax'))
+    adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    model.compile(loss = 'categorical_crossentropy', optimizer=adam, metrics =['accuracy'])
+    filepath = "devnagri_model_refined.h5"
+    checkpoint1 = ModelCheckpoint(filepath, monitor = 'val_acc', verbose = 1, save_best_only = True, mode= 'max')
+    callbacks_list = [checkpoint1]
+    return model,callbacks_list
 
-        #img = keras_process_image(gray)
+model, callbacks_list = keras_model(image_x, image_y)
+model.fit(X_train, train_y, validation_data =(X_test,test_y),epochs =20, batch_size = 64, callbacks = callbacks_list)
+scores = model.evaluate(X_test, test_y, verbose =0)
+print("CNN Error : %.2f%%" % (100-scores[1]*100))
+print_summary(model)
+model.save('devnagri.h5')
 
-        pred_probab, pred_class = keras_predict(model, gray)
-        print(pred_class, pred_probab)
 
-        print("Index got")
-        print(letter_count[pred_class])
-
-        cv2.waitKey(10000)
+#adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
